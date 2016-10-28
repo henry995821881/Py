af0860c5bc6919c7fa865576e1988929cf8dc04e
henry <123456>
2016-10-28 18:06:52 +0800

    py socket

---
+++ class1.py
@@ -0,0 +1,13 @@
+class Dog:
+	tricks =[]
+	def __init__(self,name):
+		self.name =name
+
+	def add_trick(self,trick):
+		self.tricks.append(trick)
+
+d = Dog('h')
+e = Dog('b')
+d.add_trick('roll over')
+e.add_trick('play dead')
+print d.tricks
--- request2.py
+++ request2.py
@@ -1,6 +1,6 @@
 import requests
 
-r = requests.get('http://10.10.10.31:90/cgi-bin/hello.py')
-#r = requests.get('http://10.10.10.31:90/cgi-bin/jsondata.py')
+#r = requests.get('http://10.10.10.31:90/cgi-bin/hello.py')
+r = requests.get('http://10.10.10.31:90/cgi-bin/jjson.py')
 print r.content
 
